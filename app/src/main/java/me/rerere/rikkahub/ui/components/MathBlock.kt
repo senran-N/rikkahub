13975a03cbbe2599611e906e51248e86c628646e
RE <qq1609403959@outlook.com>
2025-03-12 19:14:45 +0800

    feat: math block

--- app/src/main/java/me/rerere/rikkahub/ui/components/MathBlock.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/components/MathBlock.kt
@@ -3,29 +3,46 @@ package me.rerere.rikkahub.ui.components
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Base64
-import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
-fun MathBlock(expr: String, modifier: Modifier = Modifier) {
-    AndroidView(
-        factory = { context ->
-            MathView(context).apply {
-                renderLatex(expr, true)
-            }
-        },
-        update = { webView ->
-            webView.renderLatex(expr, true)
-        },
-        modifier = modifier,
-    )
+fun MathBlock(expr: String, modifier: Modifier = Modifier, displayMode: Boolean = true) {
+    Box(modifier) {
+        AndroidView(
+            factory = { context ->
+                MathView(context).apply {
+                    layoutParams = LayoutParams(
+                        LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT
+                    )
+                    renderLatex(expr, displayMode)
+                }
+            },
+            update = { webView ->
+                webView.renderLatex(expr, displayMode)
+            },
+            modifier = Modifier
+                .width(IntrinsicSize.Min)
+                .wrapContentSize(),
+        )
+    }
 }
 
 class MathView @JvmOverloads constructor(
@@ -45,19 +62,24 @@ class MathView @JvmOverloads constructor(
                 body {
                     margin: 0;
                     padding: 0;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
                     background-color: transparent;
+                    height: fit-content;
+                    width: fit-content;
+                }
+                .formula {
+                    width: fit-content;
+                    height: fit-content;
+                    display: inline-block;
                 }
             </style>
         </head>
         <body>
             <div class="formula" id="formula"></div>
-            <script>
+            <script type="text/javascript">
                 function renderLatex(latex, displayMode) {
+                    const decoded = atob(latex);
                     const formulaElement = document.getElementById('formula');
-                    katex.render(latex, formulaElement, {
+                    katex.render(decoded, formulaElement, {
                         throwOnError: false,
                         displayMode: displayMode
                     });
@@ -67,40 +89,50 @@ class MathView @JvmOverloads constructor(
         </html>
     """.trimIndent()
 
+    private var latex: String = ""
+    private var displayMode = true
+
     init {
-        layoutParams = LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
         with(settings) {
             javaScriptEnabled = true
-            loadWithOverviewMode = true
-            useWideViewPort = true
             builtInZoomControls = false
             displayZoomControls = false
-            domStorageEnabled = true
-            allowFileAccess = true
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
         }
 
-        setBackgroundColor(android.graphics.Color.BLUE)
-        loadData(Base64.encodeToString(katexHeader.toByteArray(), Base64.NO_PADDING), "text/html", "base64")
+        setBackgroundColor(android.graphics.Color.TRANSPARENT)
+        loadData(
+            Base64.encodeToString(katexHeader.toByteArray(), Base64.NO_PADDING),
+            "text/html",
+            "base64"
+        )
+
+        webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(message: String, lineNumber: Int, sourceID: String) {
+                println(">>> $sourceID:$lineNumber $message")
+            }
+        }
 
         webViewClient = object : WebViewClient() {
-            override fun onReceivedError(
-                view: WebView?,
-                request: WebResourceRequest?,
-                error: WebResourceError?
-            ) {
-                println(error)
-                super.onReceivedError(view, request, error)
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                refresh()
             }
         }
     }
 
     fun renderLatex(latex: String, displayMode: Boolean = true) {
-        val escapedLatex = latex.replace("\\", "\\\\").replace("\"", "\\\"")
-        val javascriptCommand = "renderLatex(\"$escapedLatex\", $displayMode)"
+        this.latex = latex
+        this.displayMode = displayMode
 
+        this.refresh()
+    }
+
+    fun refresh() {
+        val encodedLatex = Base64.encodeToString(latex.toByteArray(), Base64.NO_PADDING).trim()
+        val javascriptCommand = "javascript:renderLatex(\"$encodedLatex\", $displayMode)"
         evaluateJavascript(javascriptCommand, null)
     }
 }
--- app/src/main/java/me/rerere/rikkahub/ui/pages/ChatPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/ChatPage.kt
@@ -2,13 +2,21 @@ package me.rerere.rikkahub.ui.pages
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import me.rerere.rikkahub.ui.components.HighlightCodeBlock
@@ -114,50 +122,66 @@ fun ChatPage() {
 
             LazyColumn {
                 item {
-                    MathBlock(formula)
-                }
+                    var expr by remember {
+                        mutableStateOf("E = mc^2")
+                    }
+                    MathBlock(
+                        expr,
+                        modifier = Modifier.wrapContentSize()
+                    )
+                    TextField(
+                        value = expr,
+                        onValueChange = {
+                            expr = it
+                        },
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth()
 
-                item {
-                    HighlightCodeBlock(
-                        code = """
-                            // say hello
-                            function hello() {
-                                println("Hello, World!")
-                            }
-                            
-                            const parent = {
-                              value: 2,
-                              method() {
-                                return this.value + 1;
-                              },
-                            };
-
-                            console.log(parent.method()); // 3
-                            // 当调用 parent.method 时，“this”指向了 parent, 当调用 parent.method 时，“this”指向了 parent
-
-                            // child 是一个继承了 parent 的对象
-                            const child = {
-                              __proto__: parent,
-                            };
-                            console.log(child.method()); // 3
-                            // 调用 child.method 时，“this”指向了 child。
-                            // 又因为 child 继承的是 parent 的方法，
-                            // 首先在 child 上寻找属性“value”。
-                            // 然而，因为 child 没有名为“value”的自有属性，
-                            // 该属性会在 [[Prototype]] 上被找到，即 parent.value。
-
-                            child.value = 4; // 将 child 上的属性“value”赋值为 4。
-                            // 这会遮蔽 parent 上的“value”属性。
-                            // child 对象现在看起来是这样的：
-                            // { value: 4, __proto__: { value: 2, method: [Function] } }
-                            console.log(child.method()); // 5
-                            // 因为 child 现在拥有“value”属性，“this.value”现在表示 child.value
-
-                        """.trimIndent(),
-                        language = "js",
-                        modifier = Modifier.padding(8.dp)
                     )
                 }
+
+//                item {
+//                    HighlightCodeBlock(
+//                        code = """
+//                            // say hello
+//                            function hello() {
+//                                println("Hello, World!")
+//                            }
+//
+//                            const parent = {
+//                              value: 2,
+//                              method() {
+//                                return this.value + 1;
+//                              },
+//                            };
+//
+//                            console.log(parent.method()); // 3
+//                            // 当调用 parent.method 时，“this”指向了 parent, 当调用 parent.method 时，“this”指向了 parent
+//
+//                            // child 是一个继承了 parent 的对象
+//                            const child = {
+//                              __proto__: parent,
+//                            };
+//                            console.log(child.method()); // 3
+//                            // 调用 child.method 时，“this”指向了 child。
+//                            // 又因为 child 继承的是 parent 的方法，
+//                            // 首先在 child 上寻找属性“value”。
+//                            // 然而，因为 child 没有名为“value”的自有属性，
+//                            // 该属性会在 [[Prototype]] 上被找到，即 parent.value。
+//
+//                            child.value = 4; // 将 child 上的属性“value”赋值为 4。
+//                            // 这会遮蔽 parent 上的“value”属性。
+//                            // child 对象现在看起来是这样的：
+//                            // { value: 4, __proto__: { value: 2, method: [Function] } }
+//                            console.log(child.method()); // 5
+//                            // 因为 child 现在拥有“value”属性，“this.value”现在表示 child.value
+//
+//                        """.trimIndent(),
+//                        language = "js",
+//                        modifier = Modifier.padding(8.dp)
+//                    )
+//                }
 
 //                item {
 //                    MarkdownBlock(content)
