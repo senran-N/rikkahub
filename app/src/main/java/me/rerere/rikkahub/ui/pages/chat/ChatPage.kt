a651e886075eeb37e006d418da5d05bc82f42f29
re-ovo <qq1609403959@outlook.com>
2025-03-14 16:53:12 +0800

    chore: 完善chat页面

--- app/src/main/java/me/rerere/rikkahub/ui/pages/chat/ChatPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/chat/ChatPage.kt
@@ -6,17 +6,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import me.rerere.rikkahub.ui.components.HighlightCodeBlock
 import me.rerere.rikkahub.ui.components.MarkdownBlock
 import me.rerere.rikkahub.ui.components.chat.ChatInput
@@ -26,155 +31,152 @@ import me.rerere.rikkahub.ui.context.LocalNavController
 import me.rerere.rikkahub.ui.hooks.heroAnimation
 import org.koin.androidx.compose.koinViewModel
 
-val content = """
-# 设置
-## 这是二级标题
-### 这是三级标题
-#### 这是四级标题
-
-```java
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
-```
-
-这里是设置页面的内容
-
-文本**加粗**, *斜体*，~~删除线~~，`代码`
-
-This is inline math $\frac{1}{2}x^2 + \frac{1}{3}y^3$
-
-and block math:   
-
-$$
- f(x) = 2x+4
-$$
-
-| 标题1 | 标题2 | 标题3 | 
-| --- | --- | --- |
-| 内容1 | 内容2 | 内容3 |
-| 内容1 | 内容2 | 内容3 |
-
-> 这是一个引用
-
-[这是一个链接](https://www.google.com)
-
-- 列表1
-- 列表2
-- 列表3
-
-1. 列表1
-2. 列表2
-3. 列表3
-
-- [ ] 任务1
-- [x] 任务2
-- [ ] 任务3
-
-- [ ] 任务1
-  - [x] 子任务1
-  - [ ] 子任务2
-- [ ] 任务2
-- [ ] 任务3
-
-- [ ] 任务1
-  - [x]子任务1
-  
-this is inline code `def helloWorld()`
-  
-```javascript
-function helloWorld() {
-    console.log("Hello, World!");
-}
-```
-
-```python
-def hello_world():
-    print("Hello, World!")
-```
-""".trimIndent()
-
-val formula = """
-\f\relax{x} = \int_{-\infty}^\infty
-    \f\hat\xi\,e^{2 \pi i \xi x}
-    \,d\xi
-""".trimIndent()
-
 @Composable
 fun ChatPage(vm: ChatVM = koinViewModel()) {
     val navController = LocalNavController.current
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    TextButton(onClick = {}) {
-                        Text("DeepSeek R1")
-                    }
-                },
-                actions = {
-                    IconButton(
-                        onClick = {
-                            navController.popBackStack()
-                        }
-                    ) {
-                        Icon(MessageCirclePlus, "New Message")
-                    }
-                },
-                navigationIcon = {
-                    IconButton(
-                        onClick = {
-                            navController.popBackStack()
-                        }
-                    ) {
-                        Icon(ListTree, "Messages")
-                    }
-                }
-            )
-        },
-        bottomBar = {
-            ChatInput()
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-        ) {
-            Card(
-                modifier = Modifier.heroAnimation("setting_card"),
-                onClick = {
-                    navController.navigate("setting")
-                }
-            ) {
-                Box(
-                    modifier = Modifier.padding(8.dp)
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    Text("设置")
+                    Text("DeepSeek R1")
+                    Text("DeepSeek R1")
+                    Text("DeepSeek R1")
+                    Text("DeepSeek R1")
+                    Text("DeepSeek R1")
                 }
             }
-
-
-            LazyColumn(
-                contentPadding = PaddingValues(12.dp),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                scope.launch { drawerState.open() }
+                            }
+                        ) {
+                            Icon(ListTree, "Messages")
+                        }
+                    },
+                    title = {
+                        TextButton(onClick = {}) {
+                            Text("DeepSeek R1")
+                        }
+                    },
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                navController.popBackStack()
+                            }
+                        ) {
+                            Icon(MessageCirclePlus, "New Message")
+                        }
+                    },
+                )
+            },
+            bottomBar = {
+                ChatInput()
+            }
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
             ) {
-                item {
-                    HighlightCodeBlock(
-                        """
+                Card(
+                    modifier = Modifier.heroAnimation("setting_card"),
+                    onClick = {
+                        navController.navigate("setting")
+                    }
+                ) {
+                    Box(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("设置")
+                    }
+                }
+
+
+                LazyColumn(
+                    contentPadding = PaddingValues(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    item {
+                        HighlightCodeBlock(
+                            """
                         fun helloWorld() {
                             console.log("Hello, World!");
                         }
                     """.trimIndent(), "java"
-                    )
-                }
+                        )
+                    }
 
-                item {
-                    Card {
-                        MarkdownBlock(content, modifier = Modifier.padding(4.dp))
+                    item {
+                        Card {
+                            MarkdownBlock(content, modifier = Modifier.padding(4.dp))
+                        }
                     }
                 }
             }
         }
     }
 }
+
+val content = """
+    ## Text
+    This is a text. **This is a bold text.** *This is a italic text.* ~~This is a strikethrough text.~~
+    
+    ### Heading 3
+    This is a heading 3.
+    
+    ### Heading 4
+    This is a heading 4.
+    
+    ## Code
+    ```java
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+    }
+    ```
+    
+    ## Image
+    ![Image](https://picsum.photos/200/300)
+    
+    ## Link
+    [Link](https://www.google.com)
+    
+    ## List
+    - List item 1
+    - List item 2
+    - List item 3
+    
+    ## Table
+    | Column 1 | Column 2 | Column 3 |
+    | -------- | -------- | -------- |
+    | Row 1    | Row 1    | Row 1    |
+    | Row 2    | Row 2    | Row 2    |
+    | Row 3    | Row 3    | Row 3    |
+    
+    ## Blockquote
+    > This is a blockquote.
+    
+    ## Horizontal Rule
+    ---
+    
+    ## Empty Line
+    
+    This is a paragraph.
+    
+    This is another paragraph.
+    
+    ## Paragraph
+    This is a paragraph.
+""".trimIndent()
