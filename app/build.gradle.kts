fcc125b097f5c3d1835622ca41b22f65379b356b
RE <qq1609403959@outlook.com>
2025-04-06 11:13:03 +0800

    chore: reorganize dependencies in build.gradle.kts for clarity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,13 +62,17 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Compose
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.navigation.compose)
+
+    // DataStore
     implementation(libs.androidx.datastore.preferences)
 
     // koin
