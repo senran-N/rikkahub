bb1f082f50afdcfdf0b29395f28655f44a30c77c
RE <qq1609403959@outlook.com>
2025-04-10 17:31:11 +0800

    fix: 修复导航

--- app/src/main/java/me/rerere/rikkahub/ui/utils/ChatUtil.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/utils/ChatUtil.kt
@@ -1,13 +1,20 @@
 package me.rerere.rikkahub.ui.utils
 
+import android.util.Log
 import androidx.navigation.NavController
 import kotlin.uuid.Uuid
+
+private const val TAG = "ChatUtil"
 
 fun navigateToChatPage(
     navController: NavController,
     chatId: Uuid = Uuid.random()
 ) {
+    Log.i(TAG, "navigateToChatPage: navigate to $chatId")
     navController.navigate("chat/${chatId}") {
+        popUpTo(0) {
+            inclusive = true
+        }
         launchSingleTop = true
     }
 }
