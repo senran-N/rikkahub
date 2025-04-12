20a925a1c35fcd24350f5baf6db372e6fee9221f
RE <qq1609403959@outlook.com>
2025-04-12 14:57:16 +0800

    feat: add functions to create and delete chat files in local storage

--- app/src/main/java/me/rerere/rikkahub/utils/ChatUtil.kt
+++ app/src/main/java/me/rerere/rikkahub/utils/ChatUtil.kt
@@ -3,9 +3,13 @@ package me.rerere.rikkahub.utils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.net.Uri
 import android.util.Log
+import androidx.core.net.toFile
+import androidx.core.net.toUri
 import androidx.navigation.NavController
 import me.rerere.ai.ui.UIMessage
+import kotlin.collections.forEach
 import kotlin.uuid.Uuid
 
 private const val TAG = "ChatUtil"
@@ -31,4 +35,41 @@ fun Context.copyMessageToClipboard(message: UIMessage) {
     )
     clipboard.setPrimaryClip(clip)
     Log.i(TAG, "copyMessageToClipboard: Copied message to clipboard: ${message.text()}")
+}
+
+fun Context.createChatFiles(uris: List<Uri>): List<Uri> {
+    val newUris = mutableListOf<Uri>()
+    val dir = this.filesDir.resolve("upload")
+    if (!dir.exists()) {
+        dir.mkdirs()
+    }
+    uris.forEach { uri ->
+        val fileName = Uuid.random()
+        val newUri = dir
+            .resolve("$fileName")
+            .toUri()
+        this.contentResolver.openInputStream(uri)?.use { inputStream ->
+            this.contentResolver.openOutputStream(newUri)?.use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+        }
+        newUris.add(newUri)
+    }
+    return newUris
+}
+
+fun Context.deleteChatFiles(uris: List<Uri>) {
+    uris.forEach { uri ->
+        val file = uri.toFile()
+        if (file.exists()) {
+            file.delete()
+        }
+    }
+}
+
+fun Context.deleteAllChatFiles() {
+    val dir = this.filesDir.resolve("upload")
+    if (dir.exists()) {
+        dir.deleteRecursively()
+    }
 }
