00cb57bd8b0f603c7cdb7d7c3de172195acfa9fb
RE <qq1609403959@outlook.com>
2025-04-20 01:31:10 +0800

    chore: 添加 UiState

---
+++ app/src/main/java/me/rerere/rikkahub/utils/UiState.kt
@@ -0,0 +1,7 @@
+package me.rerere.rikkahub.utils
+
+sealed class UiState<out T> {
+    object Loading : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+    data class Error(val error: Throwable) : UiState<Nothing>()
+}
