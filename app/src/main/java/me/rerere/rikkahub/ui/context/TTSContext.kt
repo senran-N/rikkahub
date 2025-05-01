a924cc21e88eb57dadb3bfdffbf02b8b908a9539
RE <qq1609403959@outlook.com>
2025-05-01 23:18:22 +0800

    refactor: 移除未使用的 TTSContext 文件

--- app/src/main/java/me/rerere/rikkahub/ui/context/TTSContext.kt
+++
@@ -1,8 +0,0 @@
-package me.rerere.rikkahub.ui.context
-
-import android.speech.tts.TextToSpeech
-import androidx.compose.runtime.compositionLocalOf
-
-val LocalTTSService = compositionLocalOf<TextToSpeech?> {
-    error("No TTS engine provided")
-}
