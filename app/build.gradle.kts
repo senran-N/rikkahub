e93f158fbbb684d724affb1fea228cdfc2c6ba06
RE <qq1609403959@outlook.com>
2025-05-02 21:31:18 +0800

    build(app): 添加 WorkManager 依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ ksp {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
