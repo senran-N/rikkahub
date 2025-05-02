33863083f31a6d4af43b2ed46fdbeead21d6536e
RE <qq1609403959@outlook.com>
2025-05-02 21:23:56 +0800

    fix: 改回版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 17
-        versionName = "0.5.0"
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
