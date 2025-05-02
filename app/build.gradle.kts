c9d860e3b139c6307d61629ecba01c4a46d8fb81
RE <qq1609403959@outlook.com>
2025-05-02 15:07:39 +0800

    build(app): 更bump to 0.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.5.0"
+        versionCode = 17
+        versionName = "0.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
