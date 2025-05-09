7a85a024a6ce4d0f6773b74613c7a0113bdacb0d
RE <qq1609403959@outlook.com>
2025-05-10 00:16:07 +0800

    build: update versionCode to 27 and versionName to 0.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
-        versionName = "0.7.1"
+        versionCode = 27
+        versionName = "0.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
