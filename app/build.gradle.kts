967a65426d6d739c8f56875209297fad8e01f1ba
RE <qq1609403959@outlook.com>
2025-05-18 22:37:35 +0800

    chore: bump to 0.7.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 38
-        versionName = "0.7.12"
+        versionCode = 39
+        versionName = "0.7.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
