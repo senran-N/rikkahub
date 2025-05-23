c058683c9002956c757e247a0c406cabb29bbc8f
RE <qq1609403959@outlook.com>
2025-05-23 23:33:38 +0800

    refactor(model): 更新模型匹配规则

--- ai/src/main/java/me/rerere/ai/provider/Model.kt
+++ ai/src/main/java/me/rerere/ai/provider/Model.kt
@@ -75,7 +75,7 @@ fun guessModelAbilityFromModelId(modelId: String): List<ModelAbility> {
             listOf(ModelAbility.TOOL, ModelAbility.REASONING)
         }
 
-        CLAUDE_SONNET_4.containsMatchIn(modelId) -> {
+        CLAUDE_4.containsMatchIn(modelId) -> {
             listOf(ModelAbility.TOOL, ModelAbility.REASONING)
         }
 
@@ -96,5 +96,5 @@ private val GEMINI_2_5_FLASH = Regex("gemini-2.5-flash")
 private val GEMINI_2_5_PRO = Regex("gemini-2.5-pro")
 private val CLAUDE_SONNET_3_5 = Regex("claude-3.5-sonnet")
 private val CLAUDE_SONNET_3_7 = Regex("claude-3.7-sonnet")
-private val CLAUDE_SONNET_4 = Regex("claude-4-sonnet")
+private val CLAUDE_4 = Regex("claude-4")
 private val QWEN_3 = Regex("qwen-?3")
