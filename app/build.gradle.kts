b952432cb02d94257dc9ecb370c8d924e52e703a
RE <qq1609403959@outlook.com>
2025-06-02 14:47:44 +0800

    build(app): 更新应用版本0.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "0.8.0"
+        versionCode = 47
+        versionName = "0.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
