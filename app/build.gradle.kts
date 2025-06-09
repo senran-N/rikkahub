87cc2c50a3dc0e70c02aa4b7b80dccda0c37fa8a
RE <qq1609403959@outlook.com>
2025-06-09 22:50:32 +0800

    build: bump to 0.8.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 53
-        versionName = "0.8.7"
+        versionCode = 54
+        versionName = "0.8.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
